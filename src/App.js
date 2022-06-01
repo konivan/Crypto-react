329943eaebeccb61db62ed7557ef42c5561c97c5
konivan <konivan08@gmail.com>
2022-06-01 20:09:32 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -13,25 +13,35 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className='app'>
-      <div className='navbar'>
+    <div className="app">
+      <div className="navbar">
         <Navbar />
       </div>
-      <div className='main'>
+      <div className="main">
         <Layout>
-          <div className='routes'>
-              <Routes>
-                <Route  path='/' element={<Homepage />} />
-                <Route  path='/exchanges' element={<Exchanges />} />
-                <Route  path='/cryptocurrencies' element={<Cryptocurrencies />} />
-                <Route  path='/crypto/:coinId' element={<CryptoDetails />} />
-                <Route  path='/news' element={<News />} />
-              </Routes>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
           </div>
         </Layout>
-      </div>
-      <div className='footer'>
-
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}>
+            React-crypto <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
       </div>
     </div>
   );
