eed1e45ae698cbce276c870d2fb2da004f9651a0
konivan <konivan08@gmail.com>
2022-06-03 10:55:42 +0300

    fix endpoints

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,20 +1,24 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
   'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
   'X-RapidAPI-Key': '2692c4767dmshf5b65a346148613p15fd60jsnabf25d09ffd1',
 }
 
-const baseUrl = 'https://coinranking1.p.rapidapi.com/coins';
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
 
-const createRequest = (url) => ({ url, heafers: cryptoApiHeaders});
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders});
 
 export const cryptoApi = createApi({
   reducerPath: 'cryptoApi',
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getCryptos: builder.query({
-      query: () => createRequest('/exchanges')
+      query: () => createRequest('/coins')
     })
   })
 })
+
+export const {
+  useGetCryptosQuery,
+} = cryptoApi;
